c6fb8a73765ed5ce43b9efaecba294ee3299a7e8
robaddisit@gmail.com <tAlk5up!>
2019-07-30 18:05:07 +1200

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,5 +10,28 @@ from farmware_tools import app
 from farmware_tools import device
 from farmware_tools import get_config_value
 
+# Values for testing
+rows = 4
+cols = 7 
+spaceBetweenRows = 47
+spaceBetweenColumns = 45 
+startX = 310.2
+startY = 563.8
+startZ = 210.96
+sequenceBeforeMove = 'PickUpSeed' 
+sequenceAfterMove = 'PlantSeed'
+
+device.log(message='Setting variables', message_type='success')
+rows = get_config_value(farmware_name='Gridit', config_name='rows', value_type=int)
+cols = get_config_value(farmware_name='Gridit', config_name='cols', value_type=int)
+spaceBetweenRows = get_config_value(farmware_name='Gridit', config_name='spaceBetweenRows', value_type=float)
+spaceBetweenCols = get_config_value(farmware_name='Gridit', config_name='spaceBetweenCols', value_type=float)
+startX = get_config_value(farmware_name='Gridit', config_name='startX', value_type=float)
+startY = get_config_value(farmware_name='Gridit', config_name='startY', value_type=float)
+startZ = get_config_value(farmware_name='Gridit', config_name='startZ', value_type=float)
+sequenceBeforeMove = get_config_value(farmware_name='Gridit', config_name='sequenceBeforeMove', value_type=str)
+sequenceAfterMove = get_config_value(farmware_name='Gridit', config_name='sequenceAfterMove', value_type=str)
+
+device.log(message='Setting sequenceId variables', message_type='success')
 
 device.log(message='Hello Farmware!', message_type='success')
