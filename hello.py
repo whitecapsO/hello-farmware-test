cf203ba5cb0e23d39dfcd6a7cf319a34820f8af8
robaddisit@gmail.com <tAlk5up!>
2019-10-07 19:32:09 +1300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,9 @@
 A simple Farmware example that tells FarmBot to log a new message.
 '''
 
+from farmware_tools import app
 from farmware_tools import device
+from farmware_tools import env
+from farmware_tools import get_config_value
 
 device.log(message='Hello Farmware!', message_type='success')
