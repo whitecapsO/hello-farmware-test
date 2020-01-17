75ed2eeb634040845d1032487c20b98a820e90e0
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-01-17 13:24:20 +1300

    Testing

--- hello.py
+++ hello.py
@@ -7,17 +7,21 @@ from farmware_tools import app
 from farmware_tools import device
 from farmware_tools import env
 
-INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
-device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
+try:
+	INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
+	device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
 
-device.move_absolute(
-    {
-        'kind': 'coordinate',
-        'args': {'x': 10, 'y': 10, 'z': 0}
-    },
-    100,
-    {
-        'kind': 'coordinate',
-        'args': {'x': 0, 'y': 0, 'z': 0}
-    }
-)
+	device.move_absolute(
+	    {
+	        'kind': 'coordinate',
+	        'args': {'x': 10, 'y': 10, 'z': 0}
+	    },
+	    100,
+	    {
+	        'kind': 'coordinate',
+	        'args': {'x': 0, 'y': 0, 'z': 0}
+	    }
+	)
+except Exception as error:
+    device.log(repr(error))
+
