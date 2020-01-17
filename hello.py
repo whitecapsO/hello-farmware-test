d682e090cea4b3aeb1efc452a66ca4dadb76d6a2
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-01-17 22:40:27 +1300

    try again

--- hello.py
+++ hello.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python
-
-'''Hello Farmware Input
-A simple Farmware example that tells FarmBot to log a new message including the provided input.
-'''
 from farmware_tools import app
 from farmware_tools import device
 from farmware_tools import env
 from farmware_tools import get_config_value
 
 try:
- device.move_absolute(
-	device.assemble_coordinate(100, 100, 0),
-	100,
-	device.assemble_coordinate(0, 0, 0))
+    device.move_absolute(
+        device.assemble_coordinate(100, 100, 0),
+        100,
+        device.assemble_coordinate(0, 0, 0))
 
-	INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
-	device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
+    INPUT_VALUE = get_config_value(farmware_name="wc-farmware", config_name="input", value_type=str)
+    device.log(message="Hello Farmware! Input was: {}".format(INPUT_VALUE), message_type="success")
+
 except Exception as error:
     device.log(repr(error))
