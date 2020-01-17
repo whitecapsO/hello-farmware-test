90dec71a0ff893be6bd0133eefc71871cb27b440
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-01-17 14:16:48 +1300

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,12 +6,13 @@ A simple Farmware example that tells FarmBot to log a new message including the 
 from farmware_tools import app
 from farmware_tools import device
 from farmware_tools import env
+from farmware_tools import get_config_value
 
 try:
 	device.move_absolute(
 	    {
 	        'kind': 'coordinate',
-	        'args': {'x': 10, 'y': 10, 'z': 0}
+	        'args': {'x': 100, 'y': 100, 'z': 0}
 	    },
 	    100,
 	    {
