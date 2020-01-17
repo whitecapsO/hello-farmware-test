bd8fc4e5387bc6fc79db7189204a052ef901637d
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-01-17 13:16:31 +1300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,21 @@
 '''Hello Farmware Input
 A simple Farmware example that tells FarmBot to log a new message including the provided input.
 '''
-
-from farmware_tools import get_config_value, device
+from farmware_tools import app
+from farmware_tools import device
+from farmware_tools import env
 
 INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
 device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
+
+device.move_absolute(
+    {
+        'kind': 'coordinate',
+        'args': {'x': 10, 'y': 10, 'z': 0}
+    },
+    100,
+    {
+        'kind': 'coordinate',
+        'args': {'x': 0, 'y': 0, 'z': 0}
+    }
+)
