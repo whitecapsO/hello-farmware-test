37a66bd73a8a333541580e8d25e2d78f02fc64a0
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-01-24 12:25:16 +1300

    r

--- hello.py
+++ hello.py
@@ -6,35 +6,35 @@ from farmware_tools import get_config_value
 try:
     INPUT_VALUE = get_config_value(farmware_name="HelloFarmware", config_name="input", value_type=str)
     device.log(message="Hello Farmware! Input was: {}".format(INPUT_VALUE), message_type="success")
-    device.log(message="Message 1 Test", message_type="success")
-    device.log(message="Message 2 Test", message_type="success")
-    device.log(message="Message 3 Test", message_type="success")
-    device.log(message="Message 4 Test", message_type="success")
-    device.log(message="Message 5 Test", message_type="success")
+    # device.log(message="Message 1 Test", message_type="success")
+    # device.log(message="Message 2 Test", message_type="success")
+    # device.log(message="Message 3 Test", message_type="success")
+    # device.log(message="Message 4 Test", message_type="success")
+    # device.log(message="Message 5 Test", message_type="success")
 
         device.move_absolute(
         device.assemble_coordinate(100, 100, 0),
         100,
         device.assemble_coordinate(0, 0, 0))
 
-        device.move_absolute(
-        device.assemble_coordinate(200, 200, 0),
-        100,
-        device.assemble_coordinate(0, 0, 0))
+        # device.move_absolute(
+        # device.assemble_coordinate(200, 200, 0),
+        # 100,
+        # device.assemble_coordinate(0, 0, 0))
 
-        device.move_absolute(
-        device.assemble_coordinate(300, 300, 0),
-        100,
-        device.assemble_coordinate(0, 0, 0))
+        # device.move_absolute(
+        # device.assemble_coordinate(300, 300, 0),
+        # 100,
+        # device.assemble_coordinate(0, 0, 0))
 
-        device.move_absolute(
-        device.assemble_coordinate(400, 400, 0),
-        100,
-        device.assemble_coordinate(0, 0, 0))
+        # device.move_absolute(
+        # device.assemble_coordinate(400, 400, 0),
+        # 100,
+        # device.assemble_coordinate(0, 0, 0))
 
-        device.move_absolute(
-        device.assemble_coordinate(500, 500, 0),
-        100,
-        device.assemble_coordinate(0, 0, 0))
+        # device.move_absolute(
+        # device.assemble_coordinate(500, 500, 0),
+        # 100,
+        # device.assemble_coordinate(0, 0, 0))
 except Exception as error:
     device.log(repr(error))
