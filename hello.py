ad91ef83b14976b002a5c48884263e807ce64237
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-01-24 12:40:17 +1300

    r

--- hello.py
+++ hello.py
@@ -1,40 +1,11 @@
-from farmware_tools import app
-from farmware_tools import device
-from farmware_tools import env
-from farmware_tools import get_config_value
+#!/usr/bin/env python
 
-try:
-    INPUT_VALUE = get_config_value(farmware_name="hellofarmware", config_name="input", value_type=str)
-    device.log(message="Hello Farmware! Input was: {}".format(INPUT_VALUE), message_type="success")
-    # device.log(message="Message 1 Test", message_type="success")
-    # device.log(message="Message 2 Test", message_type="success")
-    # device.log(message="Message 3 Test", message_type="success")
-    # device.log(message="Message 4 Test", message_type="success")
-    # device.log(message="Message 5 Test", message_type="success")
+'''Hello Farmware Input
 
-        device.move_absolute(
-        device.assemble_coordinate(100, 100, 0),
-        100,
-        device.assemble_coordinate(0, 0, 0))
+A simple Farmware example that tells FarmBot to log a new message including the provided input.
+'''
 
-        # device.move_absolute(
-        # device.assemble_coordinate(200, 200, 0),
-        # 100,
-        # device.assemble_coordinate(0, 0, 0))
+from farmware_tools import get_config_value, device
 
-        # device.move_absolute(
-        # device.assemble_coordinate(300, 300, 0),
-        # 100,
-        # device.assemble_coordinate(0, 0, 0))
-
-        # device.move_absolute(
-        # device.assemble_coordinate(400, 400, 0),
-        # 100,
-        # device.assemble_coordinate(0, 0, 0))
-
-        # device.move_absolute(
-        # device.assemble_coordinate(500, 500, 0),
-        # 100,
-        # device.assemble_coordinate(0, 0, 0))
-except Exception as error:
-    device.log(repr(error))
+INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
+device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
