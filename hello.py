d8eba1e15e5753332002b5227c88a7f9d511c304
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-02-15 19:27:30 +1300

    Debug pos read

--- hello.py
+++ hello.py
@@ -16,26 +16,30 @@ device.log(message="Message 4 Test", message_type="success")
 device.log(message="Message 5 Test", message_type="success") """
 
 device.move_absolute(
-device.assemble_coordinate(100, 100, 0),
+device.assemble_coordinate(2218.2, 41, 0),
 100,
 device.assemble_coordinate(0, 0, 0))
 
-device.move_absolute(
-device.assemble_coordinate(200, 200, 0),
-100,
-device.assemble_coordinate(0, 0, 0))
+currentPosition = device.get_current_position()
 
-device.move_absolute(
-device.assemble_coordinate(300, 300, 0),
-100,
-device.assemble_coordinate(0, 0, 0))
+device.log(message="currentPosition:" + str(currentPosition), message_type="success")
 
-device.move_absolute(
-device.assemble_coordinate(400, 400, 0),
-100,
-device.assemble_coordinate(0, 0, 0))
+# device.move_absolute(
+# device.assemble_coordinate(200, 200, 0),
+# 100,
+# device.assemble_coordinate(0, 0, 0))
 
-device.move_absolute(
-device.assemble_coordinate(500, 500, 0),
-100,
-device.assemble_coordinate(0, 0, 0))
+# device.move_absolute(
+# device.assemble_coordinate(300, 300, 0),
+# 100,
+# device.assemble_coordinate(0, 0, 0))
+
+# device.move_absolute(
+# device.assemble_coordinate(400, 400, 0),
+# 100,
+# device.assemble_coordinate(0, 0, 0))
+
+# device.move_absolute(
+# device.assemble_coordinate(500, 500, 0),
+# 100,
+# device.assemble_coordinate(0, 0, 0))
