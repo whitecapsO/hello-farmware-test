83daf3fd5014fc85e52adf7edcec9b565e8025c2
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-03-22 14:52:21 +1300

    adas

--- hello.py
+++ hello.py
@@ -6,8 +6,9 @@ Had to reuse the original test to do this as Farmbot wouldn't load new Farmwares
 '''
 
 from farmware_tools import device
-from farmware_tools import env
+#from farmware_tools import env
 from farmware_tools import get_config_value
+import os
 
 evName = get_config_value(farmware_name='Hello Farmware Test', config_name='evName', value_type=str)
 evValue = get_config_value(farmware_name='Hello Farmware Test', config_name='evValue', value_type=str)
@@ -19,10 +20,14 @@ if evName == "default" :
 elif evName == "default" :
     device.log(message="Please enter an environment variable value" + str(evValue), message_type="success")
 else :
-    evCurrentValue = env.os.environ.get(evName, False)
+
+    #evCurrentValue = env.os.environ.get(evName, False)
+    evCurrentValue = os.environ.get(evName, False)
     if evCurrentValue == False :
         device.log(message="Creating environment variable: " + str(evName) + " value is: " + str(evValue), message_type="success")
-        env.os.environ[evName] = evValue
+        #env.os.environ[evName] = evValue
+        os.environ[evName] = evValue
     else :
-        device.log(message="Environment variable exists for name: " + str(evName) + " current value is: " + str(evCurrentValue) + " setting value to: " + str(evValue), message_type="success")
-        env.os.environ[evName] = evValue
+        device.log(message="Environment variable exists for name: " + str(evName) + " current value is: " + str(evCurrentValue) + " setting value to: " + str(evValue), message_type="success")   
+        os.environ[evName] = evValue
+       #env.os.environ[evName] = evValue
