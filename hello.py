4e2e0710e3896246d0f14925a040c81e91e7ca5e
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-03-22 16:02:26 +1300

    ad

--- hello.py
+++ hello.py
@@ -6,8 +6,8 @@ Had to reuse the original test to do this as Farmbot wouldn't load new Farmwares
 '''
 
 from farmware_tools import device
-#from farmware_tools import env
 from farmware_tools import get_config_value
+import json
 import os
 
 evName = get_config_value(farmware_name='Hello Farmware Test', config_name='evName', value_type=str)
@@ -17,17 +17,19 @@ device.log(message="Recieved environment variable name: " + str(evName) + " envi
 
 if evName == "default" :
     device.log(message="Please enter an environment variable name" + str(evValue), message_type="success")
-elif evName == "default" :
+elif evValue == "default" :
     device.log(message="Please enter an environment variable value" + str(evValue), message_type="success")
 else :
-
-    #evCurrentValue = env.os.environ.get(evName, False)
-    evCurrentValue = os.environ.get(evName, False)
-    if evCurrentValue == False :
-        device.log(message="Creating environment variable: " + str(evName) + " value is: " + str(evValue), message_type="success")
-        #env.os.environ[evName] = evValue
-        os.environ[evName] = evValue
-    else :
-        device.log(message="Environment variable exists for name: " + str(evName) + " current value is: " + str(evCurrentValue) + " setting value to: " + str(evValue), message_type="success")   
-        os.environ[evName] = evValue
-       #env.os.environ[evName] = evValue
+    configFileName = 'config.json'
+    config = {evName: evValue}
+    
+    # If the file exists delete it
+    if os.path.isfile(configFileName) :
+        os.remove(configFileName)
+        device.log(message="Config file: " + str(configFileName) + " existed so deleteing it", message_type="success")
+    
+    # Create a new file and load the config
+    with open(configFileName, 'w') as f:
+        json.dump(config, f)
+        f.close()
+    device.log(message="Created new config file: " + str(configFileName) + " and wrote environment variables to it", message_type="success")
